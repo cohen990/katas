067b8d32801addb08886c0e5cfe7660e5378f53f
Samantha Cohen <samanthacohen294@gmail.com>
2017-09-14 14:57:07 +0100

    complete leap year kata in c#

---
+++ leap-year/LeapYear/Year.cs
@@ -0,0 +1,27 @@
+﻿namespace LeapYear
+{
+    public class Year
+    {
+        private readonly int _year;
+
+        public Year(int year)
+        {
+            _year = year;
+        }
+
+        public bool IsLeapYear()
+        {
+            return DivisibleBy(4) && !(DivisibleBy(100) && NotDivisibleBy(400));
+        }
+
+        private bool NotDivisibleBy(int divisor)
+        {
+            return _year % divisor != 0;
+        }
+
+        private bool DivisibleBy(int divisor)
+        {
+            return _year % divisor == 0;
+        }
+    }
+}
