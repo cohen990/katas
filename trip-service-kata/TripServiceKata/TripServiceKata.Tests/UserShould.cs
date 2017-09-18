f02ce752e0e56903ffc3a225ff8b5192ba83125b
Samantha Cohen <samanthacohen294@gmail.com>
2017-09-18 11:49:49 +0100

    IsFriendsWith added to user

---
+++ trip-service-kata/TripServiceKata/TripServiceKata.Tests/UserShould.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace TripServiceKata.Tests
+{
+    [TestFixture]
+    public class UserShould
+    {
+        private readonly User.User Bob = new User.User();
+        private readonly User.User Paul = new User.User();
+
+        [Test]
+        public void Inform_When_Users_Are_Not_Friends()
+        {
+            var user = UserBuilder.User()
+                .FriendsWith(Bob)
+                .Build();
+
+            Assert.That(user.IsFriendsWith(Paul), Is.False);
+        }
+
+        [Test]
+        public void Inform_When_Users_Are_Friends()
+        {
+            var user = UserBuilder.User()
+                .FriendsWith(Bob, Paul)
+                .Build();
+
+            Assert.That(user.IsFriendsWith(Bob), Is.True);
+        }
+    }
+}
